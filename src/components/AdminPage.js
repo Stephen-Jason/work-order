7bf78532614ab0f9c438f8807fcef140a451533c
MrGarrson47 <62501755+MrGarrson47@users.noreply.github.com>
2020-12-16 15:23:35 +0200

    added click events for the admin's tabs

--- src/components/AdminPage.js
+++ src/components/AdminPage.js
@@ -1,37 +1,48 @@
-import {useState} from 'react'
+import { useState, useEffect } from 'react'
 import logout from '../logout.png'
 
 const AdminPage = (props) => {
 
     const [tab, setTab] = useState('Work Orders');
 
-    const handleClick = (e) => {
-        const {name, value, innerHTML} = e.target;
-        if(name === 'tab'){
-            setTab(innerHTML);
-            console.log(innerHTML);
-        }
+    useEffect(() => {
+        const tabs = document.querySelectorAll('.navTab');
+        const tabArrows = document.querySelectorAll('.navTabArrow');
+        tabs.forEach(item => item.addEventListener('click', (e) => { tabEvents(e, tabs, tabArrows, item) }));
+        
+    });
+
+    const tabEvents = (e, tabs, tabArrows, item) => {
+        setTab(e.target.innerText);
+        let tabArrow = item.children;
+        tabs.forEach(item => item.classList.remove('tabClicked'));
+        tabArrows.forEach(item => item.classList.remove('tabClicked'));
+        e.target.classList.add('tabClicked');
+        tabArrow[0].classList.add('tabClicked');
     }
 
-    return (
-        <div>
-            <header className='pageHeader'>
-                <p className='pageHeaderText'>{props.name}</p>
-                <div className='logoutCon'>
-                    <img className='logoutPic' src={logout} alt='logout'/>
-                    <button>Logout</button>
-                </div>
-            </header>
-            <nav className='navTabsCon'>
-                <div className='navTab' name='tab' onClick={(e)=>handleClick(e)}>Work Orders</div>
-                <div className='navTab' name='tab' onClick={(e)=>handleClick(e)}>Technicians</div>
-                <div className='navTab' name='tab' onClick={(e)=>handleClick(e)}>Customers</div>
-                <div className='navTab' name='tab' onClick={(e)=>handleClick(e)}>New Work Order</div>
-                <div className='navTab' name='tab' onClick={(e)=>handleClick(e)}>New Customer</div>
-                <div className='navTab' name='tab' onClick={(e)=>handleClick(e)}>New Technician</div>
-            </nav>
-        </div>
-    )
+
+
+return (
+    <div>
+        <header className='pageHeader'>
+            <p className='pageHeaderText'>{props.name}</p>
+            <div className='logoutCon'>
+                <img className='logoutPic' src={logout} alt='logout' />
+                <button>Logout</button>
+            </div>
+        </header>
+        <nav className='navTabsCon'>
+            <p className='navTab' name='tab'>Work Orders<span className='navTabArrow'></span></p>
+            <p className='navTab' name='tab'>Technicians<span className='navTabArrow'></span></p>
+            <p className='navTab' name='tab'>Customers<span className='navTabArrow'></span></p>
+            <p className='navTab' name='tab'>New Work Order<span className='navTabArrow'></span></p>
+            <p className='navTab' name='tab'>New Customer<span className='navTabArrow'></span></p>
+            <p className='navTab' name='tab'>New Technician<span className='navTabArrow'></span></p>
+        </nav>
+        {tab === 'Work Orders' ? <p>work orders page</p> : tab === 'Technicians' ? <p>technicians page</p> : tab === 'Customers' ? <p>customers page</p> : tab === 'New Work Order' ? <p>new work orders page</p> : tab === 'New Customer' ? <p>new customers page</p> : tab === 'New Technician' ? <p>new technician page</p> : null}
+    </div>
+)
 }
 
 export default AdminPage;
